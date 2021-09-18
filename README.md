bb36a0aa953367775d140abcc112342a50066759
Emmanuelle Bourigault <wolf6016@ox.ac.uk>
2021-09-18 09:31:36 +0100

    adding installation

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Head-and-Neck-Tumour-Segmentation-and-Prediction-of-Patient-Survival
+
+<p align="center"><project-description></p>
+
+Welcome to the Head and Neck Segmentation Tumour Segmentation and Prediction of Patient Survival!
+
+This project aims to provide methods to automatically segment the primary gross target volume on
+fluoro-deoxyglucose (FDG)-PET and Computed Tomography (CT) images and prediction of progression-free survival in H&N oropharyngeal cancer. We participated to the HEad and neCK TumOR Segmentation and Prediction of
+Patient Outcome Challenge 2021 (HECKTOR 2021) which creates a platform for
+comparing segmentation methods and predictions of patient survival.
+For the segmentation task, we proposed a new network based on an encoder/decoder architecture with attention mechanisms and full inter- and intra-skip connections and attention mechanisms inspired from UNet3+ to take advantage of low-level and high-level semantics at full scales. Additionally, we used Conditional Random Fields (CRF) as a post-processing step to refine the predicted segmentation maps. 
+For prediction of patient progression free survival task, we propose a Cox proportional hazard regression combining
+clinical, radiomic, and deep learning features. 
+
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/EmmanuelleB985/Head-and-Neck-Tumour-Segmentation-and-Prediction-of-Patient-Survival.git
+   ```
+2. Install the packages
+   ```sh
+   pip install requirements.txt
+   ```
+
+## Usage
+
+* For segmentation task
+```sh
+   cd src/Segmentation_Task
+```
+To train the model
+```sh
+   python main.py
+```
+
+* For Survival task
+```sh
+   cd src/Survival_Task
+```
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
---
+++ src/Segmentation_Task/metrics.py
@@ -0,0 +1,60 @@
+from scipy.spatial import cKDTree
+import numpy as np
+
+def dice(input, target):
+    axes = tuple(range(1, input.dim()))
+    bin_input = (input > 0.5).float()
+
+    intersect = (bin_input * target).sum(dim=axes)
+    union = bin_input.sum(dim=axes) + target.sum(dim=axes)
+    score = 2 * intersect / (union + 1e-3)
+
+    return score.mean()
+
+
+
+def recall(input, target):
+    axes = tuple(range(1, input.dim()))
+    binary_input = (input > 0.5).float()
+
+    true_positives = (binary_input * target).sum(dim=axes)
+    all_positives = target.sum(dim=axes)
+    recall = true_positives / all_positives
+
+    return recall.mean()
+
+
+def precision(input, target):
+    axes = tuple(range(1, input.dim()))
+    binary_input = (input > 0.5).float()
+
+    true_positives = (binary_input * target).sum(dim=axes)
+    all_positive_calls = binary_input.sum(dim=axes)
+    precision = true_positives / all_positive_calls
+
+    return precision.mean()
+
+
+
+def hausdorff_distance(image0, image1):
+    """Code copied from 
+    https://github.com/scikit-image/scikit-image/blob/main/skimage/metrics/set_metrics.py#L7-L54
+    for compatibility reason with python 3.6
+    """
+    a_points = np.transpose(np.nonzero(image0))
+    b_points = np.transpose(np.nonzero(image1))
+
+    # Handle empty sets properly:
+    # - if both sets are empty, return zero
+    # - if only one set is empty, return infinity
+    if len(a_points) == 0:
+        return 0 if len(b_points) == 0 else np.inf
+    elif len(b_points) == 0:
+        return np.inf
+
+    return max(max(cKDTree(a_points).query(b_points, k=1)[0]),
+               max(cKDTree(b_points).query(a_points, k=1)[0]))
+
+
+def dice_hausdorff_distance(input, target):
+    return (dice(input, target) + hausdorff_distance(input, target))/2
